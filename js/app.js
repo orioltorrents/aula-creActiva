9f0fbc47b6e84572e67e7bf6556eb708b6c59ebd
orioltorrents <otorrents@intermunicipal.com>
2026-02-12 15:36:07 +0100

    feat: Implement client-side application logic for user authentication, dashboard, project management, and game interaction.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 // **********************************************************
 // Substitueix aquesta URL per la que t'ha donat el Google Apps Script al fer "Deploy"
 // Exemple: 'https://script.google.com/macros/s/AKfycbx.../exec'
-const API_URL = 'https://script.google.com/macros/s/AKfycbyMp3-N6OGGvfVD1P4fcApHLnFufgekTrqZGerZyAbNOHqM8NKJfHiQwCAlSQwyK-utJg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxRn_oxpEsjG_3XMPPOsBMO7ovFO7A0cbx2bqkFQFILp0171rJOpURVyb1ABYg7VIOFYA/exec';
 
 // **********************************************************
 // ESTAT DE L'APLICACIÓ
